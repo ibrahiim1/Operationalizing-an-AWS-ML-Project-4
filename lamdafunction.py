59f9b0edac991dc8f9791ab792b929a90ebe709f
Ibrahim Ali <84272197+ibrahiim1@users.noreply.github.com>
2022-12-03 17:53:25 +0200

    Add files via upload

--- lamdafunction.py
+++ lamdafunction.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 print('Loading Lambda function')
 
 runtime=boto3.Session().client('sagemaker-runtime')
-endpoint_Name='BradTestEndpoint'
+endpoint_Name='pytorch-inference-2022-12-03-14-25-48-692'
 
 def lambda_handler(event, context):
 
